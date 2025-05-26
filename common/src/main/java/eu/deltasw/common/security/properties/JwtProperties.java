c81c3992820ec36bbc1a1325c816b42483bdc5d1
panosdim <panosdim@gmail.com>
2025-05-26 12:02:24 +0300

    Remove separate local properties files

--- common/src/main/java/eu/deltasw/common/security/properties/JwtProperties.java
+++ common/src/main/java/eu/deltasw/common/security/properties/JwtProperties.java
@@ -1,13 +1,14 @@
 package eu.deltasw.common.security.properties;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Setter
 @Getter
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String secret;
-    private long expirationTimeMs = 36000000; // 10 hours in milliseconds
+    private long expirationTimeMs = 3600000; // 1 hours in milliseconds
 }
